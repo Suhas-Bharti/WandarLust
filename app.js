049642bcb365cb015e10b19837388d27773466c5
Suhas-Bharti <suhasbharti2004@gmail.com>
2026-02-22 11:14:28 +0530

    "feat: connect mongodb with mongoose"

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 8080;
 
+const MONGO_URL = "mongodb://127.0.0.1:27017/WandarLust";
+// Connect to MongoDB
+mongoose.connect(MONGO_URL)
+  .then(() => console.log("DB connected"))
+  .catch(err => console.log(err));
+
 // Home Route
 app.get("/", (req, res) => {
-  res.send("Server is running successfully 🚀");
+  res.send("Server is running successfully!");
 });
 
 // Start Server
