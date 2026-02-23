c28e7840916979e61a334a7c8412d988a3eaac7f
Suhas-Bharti <suhasbharti2004@gmail.com>
2026-02-23 09:23:07 +0530

    "feat: add listings index route and EJS view" git push

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const Listing = require("./models/listing");
+const path = require("path");
 
 const app = express();
 const PORT = 8080;
@@ -12,30 +13,39 @@ mongoose.connect(MONGO_URL)
   .then(() => console.log("DB connected"))
   .catch(err => console.log(err));
 
+// Set EJS as the templating engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 // Home Route
 app.get("/", (req, res) => {
   res.send("Server is running successfully!");
 });
 
 // Import listing routes
-app.get("/testListing", async (req, res) => {
-  let sampleListing = new Listing({
-    title: "Beautiful Beach House",
-    description: "A stunning beach house with ocean views and modern amenities.",
-    image: "",
-    price: 1200,
-    location: "Malibu",
-    country: "United States"
-  });
-
-  try {
-    await sampleListing.save();
-    res.send("Sample listing created successfully!");
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Error creating sample listing");
-  }
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", {allListings});
 });
+
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "Beautiful Beach House",
+//     description: "A stunning beach house with ocean views and modern amenities.",
+//     image: "",
+//     price: 1200,
+//     location: "Malibu",
+//     country: "United States"
+//   });
+
+//   try {
+//     await sampleListing.save();
+//     res.send("Sample listing created successfully!");
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).send("Error creating sample listing");
+//   }
+// });
 
 // Start Server
 app.listen(PORT, () => {
