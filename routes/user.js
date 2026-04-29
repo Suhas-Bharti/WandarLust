54742ee3f3b5b22c59625439d891f0234125e93b
Suhas Bharti <suhasbharti2004@gmail.com>
2026-04-30 00:10:00 +0530

    feat: add login route and view for user authentication

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const wrapAsync = require('../utils/wrapAsync');
 const router = express.Router();
 const User = require("../models/user");
+const passport = require("passport");
 
 router.get("/signup", (req, res) => {
     res.render("users/signup");
@@ -22,5 +23,14 @@ router.post("/signup", wrapAsync(async (req, res) => {
 
 }));
 
+
+router.get("/login", (req, res) => {
+    res.render("users/login");
+});
+
+router.post("/login", passport.authenticate("local", {failureRedirect: "/login", failureFlash: true}), async (req, res) => {
+    req.flash("success", "Welcome back!");
+    res.redirect("/listings");
+});
 
 module.exports = router;
