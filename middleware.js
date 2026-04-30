3e5a12b59f7341d641fb244f76947a0f6701aff0
Suhas Bharti <suhasbharti2004@gmail.com>
2026-04-30 17:38:10 +0530

    feat: enhance user authentication flow with redirect URL handling

--- middleware.js
+++ middleware.js
@@ -1,7 +1,15 @@
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()) {
+        req.session.redirectUrl = req.originalUrl; // Store the original URL to redirect after login    
         req.flash("error", "You must be logged in to do that!");
         return res.redirect("/login");
+    }
+    next();
+};
+
+module.exports.saveRedirectUrl = (req, res, next) => {
+    if (req.session.redirectUrl) {
+        res.locals.redirectUrl = req.session.redirectUrl; // Make the redirect URL available in response locals
     }
     next();
 };
