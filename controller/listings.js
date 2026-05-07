056f50c3ac75b7461bff3fbbe3e4c68cdd40adc1
Suhas Bharti <suhasbharti2004@gmail.com>
2026-05-07 16:57:41 +0530

    feat: refactor listing routes to use controller functions for better organization and maintainability

---
+++ controller/listings.js
@@ -0,0 +1,70 @@
+const Listing = require("../models/listing");
+
+// Controller functions for listings
+module.exports.index = async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+};
+
+
+// Render form to create new listing
+module.exports.renderNewForm = (req, res) => {
+    res.render("listings/new.ejs");
+};
+
+
+// Show single listing with populated reviews and owner
+module.exports.showListing = async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id)
+        .populate({
+            path: "reviews", populate: { path: "author" },
+        })
+        .populate("owner");
+    if (!listing) {
+        req.flash("error", "Listing not found!");
+        return res.redirect("/listings");
+    }
+    console.log(listing);
+    res.render("listings/show.ejs", { listing });
+};
+
+
+// Create new listing in database
+module.exports.createListing = async (req, res) => {
+    const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id; // Set owner to currently logged in user
+    await newListing.save();
+    req.flash("success", "New listing created!");
+    res.redirect("/listings");
+};
+
+
+// Render form to edit existing listing
+module.exports.showEditForm = async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    if (!listing) {
+        req.flash("error", "Listing not found!");
+        return res.redirect("/listings");
+    }
+    res.render("listings/edit.ejs", { listing });
+};
+
+
+// Update listing details in database
+module.exports.updateListing = async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    req.flash("success", "Listing updated successfully!");
+    res.redirect(`/listings/${id}`);
+};
+
+
+// Delete listing from database (Cascade delete handled in model middleware)
+module.exports.deleteListing = async (req, res) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    req.flash("success", "Listing deleted successfully!");
+    res.redirect("/listings");
+};
